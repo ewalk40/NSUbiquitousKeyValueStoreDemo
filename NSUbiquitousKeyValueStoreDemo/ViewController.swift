44db36a0659c9acaa8f1eee1ccc0a7fdf0ce119f
Eric Walker <walkereric10@gmail.com>
2019-08-04 13:01:19 -0400

    First commit (hopefully I don't need more)

--- NSUbiquitousKeyValueStoreDemo/ViewController.swift
+++ NSUbiquitousKeyValueStoreDemo/ViewController.swift
@@ -1,20 +1,33 @@
-//
-//  ViewController.swift
-//  NSUbiquitousKeyValueStoreDemo
-//
-//  Created by Eric Walker on 8/4/19.
-//  Copyright © 2019 Eric Walker. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dataLbl: UILabel!
+    @IBOutlet weak var dataTextField: UITextField!
+    
+    var keyValStore = NSUbiquitousKeyValueStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveBtn(_ sender: Any) {
+        
+        let data = dataTextField.text
+        
+        keyValStore.set(data, forKey: "data")
+        keyValStore.synchronize()
+        
+    }
+    
+    @IBAction func loadBtn(_ sender: Any) {
+        
+        let newData = keyValStore.string(forKey: "data")
+        
+        dataLbl.text = newData
+        
+    }
+    
 }
 
